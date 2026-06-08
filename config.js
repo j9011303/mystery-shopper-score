cbb91fdc5cf2ed0952bc3fb06b933f298b4181f6
JasonWu <69780773+j9011303@users.noreply.github.com>
2026-06-08 17:20:46 +0800

    Add config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+window.MYSTERY_SHOPPER_CONFIG = {
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbw9wU54LVTN6KLUmtSpEFr6EggLwHPbV1J6OiyqahGs3JSvwKVT2M4-tVQ1mCFEqnUZIw/exec",
+  FORM_TOKEN: "",
+};
